b5f6fcc28cc846075d590b7bb2d5a03d6cd27052
PowerDev <140966320+PowerzinBR@users.noreply.github.com>
2023-11-04 10:50:05 -0300

    Update type.js

--- utils/type.js
+++ utils/type.js
@@ -1,4 +1,4 @@
-const textToType = "Oi, eu sou o Power. Eu faço bots na plataforma Discord usando Python e faço websites. Gosto muito de pudim, tenho mais de 15 anos e eternamente odeio NodeJS. Porque sim, eu não sei mexer muito com JavaScript, principalmente NodeJS, mas em geral, eu prefiro Python. <br>E você?<br>Qual seu nome<br>";
+const textToType = "Oi, eu sou o Power. Eu faço bots na plataforma Discord usando Python e faço websites. Gosto muito de pudim, tenho mais de 15 anos e eternamente odeio NodeJS. Porque sim, eu não sei mexer muito com JavaScript, principalmente NodeJS, mas em geral, eu prefiro Python. ";
         const typingSpeed = 70;
         const typingElement = document.getElementById("typing-text");
 
